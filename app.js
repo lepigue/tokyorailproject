dfb5a901564a2c19654eb12f540646e02257f127
Lauren Pigue <piguel@flip2.engr.oregonstate.edu>
2022-10-27 16:10:30 -0700

    Changed a bunch of links, updated the app.js to render all the new pages, etc..

--- app.js
+++ app.js
@@ -25,25 +25,76 @@ app.get('/', function(req, res)
         res.render('index');                   
     });               
 
-app.get('/stations', function(req, res)
+app.get('/index', function(req, res)
     {
-        res.render('stations');                    
-    });               
-
-app.get('/lines', function(req, res)
+        res.render('index');                   
+    });      
+    
+app.get('/line_view', function(req, res)
     {
-        res.render('lines');                   
-    });  
-       
-app.get('/operators', function(req, res)
-    {
-        res.render('operators');                    
+        res.render('line_view');                    
     }); 
 
-app.get('/trains', function(req, res)
+app.get('/line_edit', function(req, res)
     {
-        res.render('trains');                    
+        res.render('line_edit');                    
     }); 
+
+app.get('/line_template', function(req, res)
+    {
+        res.render('line_template');                    
+    }); 
+
+app.get('/operator_edit', function(req, res)
+    {
+        res.render('operator_edit');                    
+    }); 
+
+app.get('/operator_template', function(req, res)
+    {
+        res.render('operator_template');                    
+    }); 
+
+app.get('/operator_view', function(req, res)
+    {
+        res.render('operator_view');                    
+    }); 
+
+app.get('/schedule_edit', function(req, res)
+    {
+        res.render('schedule_edit');                    
+    }); 
+
+app.get('/station_edit', function(req, res)
+    {
+        res.render('station_edit');                    
+    }); 
+
+app.get('/station_template', function(req, res)
+    {
+        res.render('station_template');                    
+    }); 
+    
+app.get('/station_view', function(req, res)
+    {
+        res.render('station_view');                    
+    }); 
+
+app.get('/train_edit', function(req, res)
+    {
+        res.render('train_edit');                    
+    }); 
+
+app.get('/train_template', function(req, res)
+    {
+        res.render('train_template');                    
+    }); 
+
+app.get('/train_view', function(req, res)
+    {
+        res.render('train_view');                    
+    }); 
+
 
 /*
     LISTENER
