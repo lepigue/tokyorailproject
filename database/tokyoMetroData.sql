3df102690a9047e952a62b80548b2c3c7660ce9d
Tyler Dennis <ctdennis@protonmail.com>
2022-11-12 17:30:29 -0800

    Updated data queries

--- database/tokyoMetroData.sql
+++ database/tokyoMetroData.sql
@@ -50,6 +50,7 @@ CREATE TABLE `Operators` (
     `last_name` varchar(30)NOT NULL,
     `phone_number` int(11) NOT NULL, 
     `email` varchar(40) NOT NULL,
+    `train_code` int,
     PRIMARY KEY (`operator_ID`)
 );
 
@@ -77,10 +78,12 @@ ALTER TABLE `Schedules`
 ADD FOREIGN KEY (`station_code`) REFERENCES `Stations`(`station_ID`) ON DELETE CASCADE,
 ADD FOREIGN KEY (`train_code`) REFERENCES `Trains`(`train_ID`) ON DELETE CASCADE;
 
+ALTER TABLE `Operators`
+ADD FOREIGN KEY (`train_code`) REFERENCES `Trains`(`train_ID`) ON DELETE CASCADE;
+
 ALTER TABLE `Lines_has_Stations`
 ADD FOREIGN KEY (`lines_line_id`) REFERENCES `Lines`(`line_ID`) ON DELETE CASCADE,
 ADD FOREIGN KEY (`stations_station_id`) REFERENCES `Stations`(`station_ID`) ON DELETE CASCADE;
-
 
 -- INSERT INTO TABLES
 
@@ -90,23 +93,39 @@ VALUES  ('500 Series', '2021-04-15'),
         ('E2 Series', '2021-10-21'),
         ('E3 Series', '2021-11-11');
 
-INSERT INTO `Stations` (`location_name`)
-VALUES  ('Ginza'),
-        ('Shinjuku'),
-        ('Ueno'),
-        ('Asakusa'),
-        ('Meiji-Jingumae'),
-        ('Shibuya'),
-        ('Shimbashi'),
-        ('Omote-sando');
-
 INSERT INTO `Lines` (`line_name`)
-VALUES  ('Hibiya'),
-        ('Tozai'),
+VALUES  ('Ginza'),
+        ('Marunouchi'),
+        ('Hibiya'),
+        ('Tōzai'),
         ('Chiyoda'),
+        ('Yūrakuchō'),
+        ('Hanzõmon'),
+        ('Namboku'),
         ('Fukutoshin');
 
-INSERT INTO `Schedules` (`arrival_time`, `departure_time`)
+INSERT INTO `Stations` (`location_name`, `station_no`, `line_code`)
+VALUES  ('Shibuya', 1, 1),
+        ('Omote-sando', 2, 1),
+        ('Gaiemmae', 3, 1),
+        ('Aoyama-itchome', 4, 1),
+        ('Akasaka-mitsuke', 5, 1),
+        ('Tameike-sanno', 6, 1),
+        ('Toranomon', 7, 1),
+        ('Shimbashi', 8, 1),
+        ('Ginza', 9, 1),
+        ('Kyobashi', 10, 1),
+        ('Nihombashi', 11, 1),
+        ('Mitsukoshimae', 12, 1),
+        ('Kanda', 13, 1),
+        ('Suehirocho', 14, 1),
+        ('Ueno-hirojoki', 15, 1),
+        ('Ueno', 16, 1),
+        ('Inaricho', 17, 1),
+        ('Tawaramachi', 18, 1),
+        ('Asukasa', 19, 1);
+
+INSERT INTO `Schedules` (`arrival_time`, `departure_time`, `station_code`, `train_code`)
 VALUES  ('10:55:00', '11:00:00'),
         ('09:25:00', '09:30:00'),
         ('07:55:00', '08:00:00'),
@@ -118,10 +137,33 @@ VALUES  ('Haruto', 'Tanaka', 499274029, 'tanakah@metro.tokyo.jp'),
         ('Chiyo', 'Fujita', 499207058, 'fujitac@metro.tokyo.jp'),
         ('Isamu', 'Sano', 499288428, 'noguchii@metro.tokyo.jp');
 
+INSERT INTO `Lines_Has_Stations` (`stations_station_id`, `lines_line_id`)
+VALUES  ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4');
 
 -- Add mock data into the db
 -- For this simple example, no sub-queries were used,
 -- but these will be utilized in our final project
+
+-- Update start_station and end_station for all Lines
+
+UPDATE `Lines`
+SET  start_station=1, end_station=5
+WHERE line_ID=1;
+
+UPDATE `Lines`
+SET start_station=2, end_station=6
+WHERE line_ID=2;
+
+UPDATE `Lines`
+SET start_station=3, end_station=7
+WHERE line_ID=3;
+
+UPDATE `Lines`
+SET start_station=4, end_station=8
+WHERE line_ID=4;
 
 -- Update line_code for all Trains
 
@@ -141,40 +183,6 @@ UPDATE `Trains`
 SET line_code=4
 WHERE train_ID=4;
 
--- Update line_code for all Stations
-
-UPDATE `Stations`
-SET line_code=1
-WHERE station_ID=1;
-
-UPDATE `Stations`
-SET line_code=2
-WHERE station_ID=2 ;
-
-UPDATE `Stations`
-SET line_code=3
-WHERE station_ID=3;
-
-UPDATE `Stations`
-SET line_code=4
-WHERE station_ID=4;
-
-UPDATE `Stations`
-SET line_code=1
-WHERE station_ID=5;
-
-UPDATE `Stations`
-SET line_code=2
-WHERE station_ID=6;
-
-UPDATE `Stations`
-SET line_code=3
-WHERE station_ID=7;
-
-UPDATE `Stations`
-SET line_code=4
-WHERE station_ID=8;
-
 -- Update station_code and train_code for all Schedules
 
 UPDATE `Schedules`
@@ -193,23 +201,24 @@ UPDATE `Schedules`
 SET station_code=4, train_code=4
 WHERE schedule_ID=4;
 
--- Update start_station and end_station for all Lines
 
-UPDATE `Lines`
-SET  start_station=1, end_station=5
-WHERE line_ID=1;
+-- Update train_code for all Operators
 
-UPDATE `Lines`
-SET start_station=2, end_station=6
-WHERE line_ID=2;
+UPDATE `Operators`
+SET train_code=1
+WHERE operator_ID=1;
 
-UPDATE `Lines`
-SET start_station=3, end_station=7
-WHERE line_ID=3;
+UPDATE `Operators`
+SET train_code=2
+WHERE operator_ID=2;
 
-UPDATE `Lines`
-SET start_station=4, end_station=8
-WHERE line_ID=4;
+UPDATE `Operators`
+SET train_code=3
+WHERE operator_ID=3;
+
+UPDATE `Operators`
+SET train_code=4
+WHERE operator_ID=4;
 
 --  RUN SOME SQL TEST COMMANDS
 
