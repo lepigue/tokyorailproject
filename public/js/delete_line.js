a629ae7ef39c61f834e9846b54fb0e45c2dd766c
Lauren Pigue <piguel@flip2.engr.oregonstate.edu>
2022-11-15 21:24:35 -0800

    Table added to lines, Create and Delete working for Trains

---
+++ public/js/delete_line.js
@@ -0,0 +1,52 @@
+function deleteLine(lineID) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        line_ID: lineID
+    };
+    //console.log(data)
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete_line", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            deleteRow(lineID);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(lineID){
+    let table = document.getElementById("line_table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == lineID) {
+            table.deleteRow(i);
+            deleteDropDownMenu(lineID);
+            break;
+       }
+    }
+}
+
+function deleteDropDownMenu(personID){
+    let selectMenu = document.getElementById("del_dropdown");
+    for (let i = 0; i < selectMenu.length; i++){
+      if (Number(selectMenu.options[i].value) === Number(personID)){
+        selectMenu[i].remove();
+        break;
+      } 
+  
+    }
+  }
